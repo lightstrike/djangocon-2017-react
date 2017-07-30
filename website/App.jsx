c6d1bd696998824860c976b2f1511290b89bc43f
Jack McCloy <mccloy@me.com>
2017-07-30 14:38:36 -0400

    chore(frontend): Turns App.jsx into a functional component

--- website/App.jsx
+++ website/App.jsx
@@ -1,26 +1,22 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Header from 'website/components/stateful/Header';
 import HomePage from 'website/components/stateful/HomePage';
 import TalkPage from 'website/components/stateful/TalkPage';
 import Footer from 'website/components/stateless/Footer';
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div>
-          <Header heading="Great Django Talks" />
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route path="/:slug" component={TalkPage} />
-          </Switch>
-          <Footer />
-        </div>
-      </BrowserRouter>
-    );
-  }
-}
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <Header heading="Great Django Talks" />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/:slug" component={TalkPage} />
+      </Switch>
+      <Footer />
+    </div>
+  </BrowserRouter>
+);
 
 export default App;
